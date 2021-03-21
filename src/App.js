f848806dba578568be6f94892e66462ddfcf491e
Mahmoud <mymafm0405@gmail.com>
2021-03-21 19:56:48 +0300

    Monster app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [
+        {
+          id: "as1",
+          name: "Frankshtine",
+        },
+        {
+          id: "as2",
+          name: "Dracula",
+        },
+        {
+          id: "as3",
+          name: "Zombie",
+        },
+      ],
+    };
+  }
+  render() {
+    return (
+      <div className="App">
+        {this.state.monsters.map((monster) => (
+          <h1 key={monster.id}>{monster.name}</h1>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
